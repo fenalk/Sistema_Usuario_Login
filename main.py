ebedf7842b03bd0e6107e2b6f944cc8269333599
devfenalk <ti.fernandaborges@gmail.com>
2023-10-01 17:11:09 -0300

    definindo as opções de usuário

--- main.py
+++ main.py
@@ -3,4 +3,28 @@
 
 import csv
 
+def cadastrar_usuario(nome, email, senha):
+    with open('usuarios.csv','a', newline='') as arquivo_csv:
+        writer = csv.writer(arquivo_csv)
+        writer.writerow([nome, email, senha])
+    print("Usuário cadastrado com sucesso!")
+
+#verificar se o usuário já é cadastrado no sistema
+def verificar_usuario(email, senha):
+    with open('usuarios.csv', 'r') as arquivo_csv:
+        reader = csv.reader(arquivo_csv)
+        for row in reader:
+            if row[1] == email and row[2] == senha:
+                return True
+    return False
+
+def main():
+    while True:
+        print('1. Cadastrar Usuário')
+        print('2. Entrar')
+        print('3. Sair')
+        escolha = input('Escolha uma opção: ')
+
+
+
 
